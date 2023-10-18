0fcb8a50ed021f40f2fdf63e9f21f060d27a5b87
AnisBenFerhat <anisbenferhat@gmail.com>
2023-10-18 17:19:07 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Correct the import path
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
