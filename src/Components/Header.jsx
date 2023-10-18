a3612d17084562959944d7764966fd11b91b84bb
AnisBenFerhat <anisbenferhat@gmail.com>
2023-10-18 18:39:07 +0200

    feat: Update header styles
    - Adjusted font size for title and subtitle on laptop view and adapted a
    scale for consistency
    - Improved alignment and text size on tablet and smartphone views
    - Updated image size and spacing in the header for a better layout
    - Minified size of the png

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <>
+      <header className='header'>
+        <img
+          className='header--img'
+          src='/images/header-logo.png'
+          alt='Yao Ming meme face with a surprised expression.'
+        />
+        <h2 className='header--title'>Meme Generator</h2>
+        <h4 className='header--subtitle'>React Project</h4>
+      </header>
+    </>
+  );
+}
+
+export default Header;
