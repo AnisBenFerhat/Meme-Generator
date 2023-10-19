578005ba3b32b9e74318cd38d669e3272c541414
AnisBenFerhat <anisbenferhat@gmail.com>
2023-10-19 10:40:10 +0200

    feat: Create Form Component and General Style

---
+++ src/Components/Form.jsx
@@ -0,0 +1,13 @@
+function Form() {
+  return (
+    <main>
+      <form className='form'>
+        <input type='text' className='form--input' placeholder='shut up' />
+        <input type='text' className='form--input' placeholder='and take my money' />
+        <button className='form--button'>A New Meme Image</button>
+      </form>
+    </main>
+  );
+}
+
+export default Form;
